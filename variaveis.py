012764ce0f2b79a68f2a76d9203809c4c2a62f5a
Kalebe <kalebepereira73@gmailcom>
2026-09-05 17:48:36 -0300

    {Criação} _ Aprendendo sobre os tipos de dados e as operações matemáticas

---
+++ variaveis.py
@@ -0,0 +1,73 @@
+                    # TIPOS DE DADOS
+
+# No Python, temos os seguintes tipos de dados:
+        # int -> Tipo inteiro - Usado para número Naturais(1,2,3,4...) .
+
+        # string -> Tipo Caracter - Usado para texto: "Olá".
+        
+        # float ->  Tipo de ponto flutuante - Usado para número com casas decimal
+        
+        # bool -> Tipo boleano - Utilizado quando  há uma condição de verdadeiro ou falso
+        
+# Exemplos
+
+numero = 1
+texto = "Olá, Mundo!"
+ponto = 3.1415
+boleano = True
+
+print(numero)
+print(texto)
+print(ponto)
+print(boleano)
+
+# ----------------------------------------------------------------------------------------------
+
+                        # OPERADORES
+
+    # No python, existem os operadores matemáticos básicos. Eles funcionam em variáveis e algumas funções, como o 'print'.
+    
+        # SOMA
+        
+        # O operador de soma funciona do mesmo jeito da matemática, ele utiliza o sinal de mais (+) para efetuar operações. Exemplo:
+print(5 + 5)
+soma = 2 + 6
+
+        # SUBTRAÇÃO
+        
+        # A subtração é feita de mesmo modo do que a matemática, sendo um traço horizontal(-). Exeplo:
+print(5 - 3)
+subtracao = 10 - 7
+
+        # MULTIPLICAÇÃO
+        
+        # A multiplicação é um símbolo diferente da matemática. Na computação, possui-se o sinal de asterico(*) como o sinal de multiplicação. Exemplo:
+print(5 * 5)
+multiplicacao = 9 * 9
+
+        # DIVISÃO
+
+        # A divisão possui o símbolo simplificado da matemática, caracterizado por uma barra(/). Exemplo:
+print(1 / 3)
+divisao = 10 / 4
+
+        # DIVISÃO INTEIRA
+        
+        # Chegado nos operadores computacionais, temos a divisão inteira. Ela efetua a divisão normalmente mas, ela desconsidera a parte fracionada da divisão, pegando somente a parte inteira.Possui-se o simbolo de duas barras. Exemplo:
+print(1//3)
+divisaoInteira = 9//4
+
+        # RESTO DA DIVISÃO
+        
+        # O resto da divisão é meio complicado de se entender. Nela é efetuado uma operação de divisão, mas será considerado não o resultado, mas, o valor final após a divisão. Possui o símbolo de porcentagem(%). Exemplo:
+print(10 % 2)
+restoDivisao = 10 % 3
+
+            # POTENCIAÇÃO
+            
+        # A pontenciação já é conhecida na matemática, mas ela possui outro símbolo na computação, sendo caracterizado por dois asterisco(**), sendo o primeiro número é a base e o segundo é a potencia. Exemplo:
+print(3**2)
+potencia = 10**2
+
+
+# NOTA: Pode-se misturar operações, igual a uma expressão matemática.
